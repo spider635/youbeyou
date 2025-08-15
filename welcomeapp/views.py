b201ef477b22696db17813db10f0ef2d7d10d086
Yanbang Jia <yanbangjia@gmail.com>
2025-08-15 11:04:57 +0800

    create first app.

--- welcomeapp/views.py
+++ welcomeapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# 最简单的视图函数
+def hello(request):
+    return HttpResponse("Hello, Django!")
+
+
+# 带模板的视图
+def home(request):
+    context = {
+        "title": "欢迎页面",
+        "message": "这是我的第一个Django模板页面!",
+        "items": ["苹果", "香蕉", "橙子"],
+    }
+    return render(request, "myapp/home.html", context)
